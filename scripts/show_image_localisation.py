0b0cc7f1f02b972147f7a7957a2121ea8e02e047
Dan Oneata <dan.oneata@gmail.com>
2023-04-12 15:11:39 +0300

    WIP Display more information

--- scripts/show_image_localisation.py
+++ scripts/show_image_localisation.py
@@ -13,6 +13,7 @@
 # - [ ] check my scores agree with Leanne's
 #
 
+import json
 import pdb
 import pickle
 import sys
@@ -25,9 +26,12 @@ import numpy as np
 import streamlit as st
 import torch
 
+from toolz import first
 from torch import nn
 from torchvision import transforms
 from torchvision.models import alexnet
+
+from pycocotools.coco import COCO
 
 sys.path.insert(0, ".")
 from models.multimodalModels import mutlimodal as AudioModel
@@ -46,8 +50,19 @@ MODEL_DIR = Path(
     "model_metadata/spokencoco_train/AudioModel-Transformer_ImageModel-Resnet50_ArgumentsHash-2560499dfc_ConfigFile-params"
 )
 
+
+BASE_DIR = Path("/mnt/private-share/speechDatabases")
+AUDIO_COCO_DIR = BASE_DIR / "spoken-coco"
+IMAGE_COCO_DIR = BASE_DIR / "coco"
+
+
 with open(MODEL_DIR / "args.pkl", "rb") as f:
     ARGS = pickle.load(f)
+
+
+def load_captions():
+    with open(AUDIO_COCO_DIR / "SpokenCOCO_val.json", "r") as f:
+        return json.load(f)["data"]
 
 
 class MattNet(nn.Module):
@@ -108,30 +123,17 @@ class MattNet(nn.Module):
         return score, att
 
 
-# from pycocotools.coco import COCO
-BASE_DIR = Path("/mnt/private-share/speechDatabases")
-audio_coco_dir = BASE_DIR / "spoken-coco"
-image_coco_dir = BASE_DIR / "coco"
-
-# coco = COCO(coco_path / "annotations" / "instances_val2014.json")
-# cat_ids = coco.getCatIds(catNms=["zebra"])
 # img_ids = coco.getImgIds(catIds=cat_ids)
 # img = coco.loadImgs(img_ids[0])[0]
 # img_path = coco_path / "val2014" / img["file_name"]
-# ann_ids = coco.getAnnIds(imgIds=img["id"], catIds=cat_ids)
-# anns = coco.loadAnns(ann_ids)
 # st.image(str(img_path))
 # for ann in anns:
 #     st.image(255 * coco.annToMask(ann))
 # pdb.set_trace()
 
-# st.image("/mnt/private-share/speechDatabases/coco/val2014/COCO_val2014_000000348730.jpg")
-
-# image_embeddings = np.load("data/image_embeddings.npz", allow_pickle=True)["embeddings"].item()
-# pdb.set_trace()
-
 concepts = load_concepts()
 alignments = load_alignments(concepts)
+captions_data = load_captions()
 
 pad_kwargs = {
     "target_length": ARGS["audio_config"].get("target_length", 1024),
@@ -152,38 +154,58 @@ print("Load model...")
 mattnet = MattNet()
 mattnet.eval()
 
-EPISODE_NO = 0
-QUERY_CONCEPT = "broccoli"
+with st.sidebar:
+    query_concept = st.selectbox("query concept", concepts)
+    episode_no = st.number_input(
+        "episode no.", min_value=0, max_value=1000, format="%d", step=1
+    )
 
 episodes = np.load("data/test_episodes.npz", allow_pickle=True)["episodes"].item()
-audio_query, _ = episodes[EPISODE_NO]["queries"][QUERY_CONCEPT]
+audio_query, _ = episodes[episode_no]["queries"][query_concept]
 image_matching_set = list(episodes["matching_set"].keys())
 
-audio_path = audio_coco_dir / audio_query
+coco = COCO(IMAGE_COCO_DIR / "annotations" / "instances_val2014.json")
+coco_category_ids = coco.getCatIds(catNms=[query_concept])
+
+audio_path = AUDIO_COCO_DIR / audio_query
 audio_name = audio_path.stem
 
 print("Load audio...")
 audio, _ = load_audio(
-    audio_path, alignments[audio_name][QUERY_CONCEPT], ARGS["audio_config"]
+    audio_path, alignments[audio_name][query_concept], ARGS["audio_config"]
 )
 audio, _ = pad_audio(audio, **pad_kwargs)
 
-st.markdown(f"`{audio_name}`")
+caption = first(
+    [
+        caption["text"]
+        for entry in captions_data
+        for caption in entry["captions"]
+        if caption["wav"] == audio_query
+    ]
+)
+
+st.markdown(f"query concept: `{query_concept}`")
+st.markdown(f"audio name: `{audio_name}`")
 st.audio(str(audio_path))
+st.markdown("caption:")
+st.code(caption)
 st.markdown("---")
 
 for i, image_file in enumerate(image_matching_set):
-    if QUERY_CONCEPT not in episodes["matching_set"][image_file]:
+    contains_query_based_on_caption = query_concept in episodes["matching_set"][image_file]
+
+    if not contains_query_based_on_caption:
         continue
 
-    image_path = image_coco_dir / image_file
+    image_path = IMAGE_COCO_DIR / image_file
     image_name = image_path.stem
 
-    print("Load image...")
+    # print("Load image...")
     image = load_image(image_path, **image_transforms)
     image = image.unsqueeze(0)
 
-    print("Predict...")
+    # print("Predict...")
     with torch.no_grad():
         score, attention = mattnet(audio, image)
 
@@ -196,12 +218,40 @@ for i, image_file in enumerate(image_matching_set):
     attention_image = attention_image.resize(IMG_SIZE, Image.Resampling.BICUBIC)
     attention_image = np.clip(np.array(attention_image), 0, 1)
 
-    st.markdown(f"`{image_name}`")
+    st.markdown(f"image name: `{image_name}`")
     cols = st.columns(2)
+
+    cols[0].markdown("image")
     cols[0].image(str(image_path))
+
+    cols[1].markdown("attention")
     cols[1].image(attention_image)
 
-    st.markdown("Score: {:3f}".format(score.item()))
-    st.markdown("---")
+    captions_for_image = [
+        caption["text"]
+        for entry in captions_data
+        for caption in entry["captions"]
+        if entry["image"] == image_file
+    ]
+    captions_for_image_str = "\n".join(f"  - `{c}`" for c in captions_for_image)
 
-pdb.set_trace()
+    coco_image_id = [int(image_name.split("_")[-1])]
+    coco_annot_ids = coco.getAnnIds(imgIds=coco_image_id, catIds=coco_category_ids)
+    coco_annots = coco.loadAnns(coco_annot_ids)
+    contains_query_based_on_image = len(coco_annots) > 0
+
+    st.markdown(
+        """
+- score (maximum of attetion): {:.3f}
+- contains query based on caption: {:s}
+- contains query based on image: {:s}
+- captions:
+{:s}
+""".format(
+            score.item(),
+            "✓" if contains_query_based_on_caption else "✗",
+            "✓" if contains_query_based_on_image else "✗",
+            captions_for_image_str,
+        )
+    )
+    st.markdown("---")
