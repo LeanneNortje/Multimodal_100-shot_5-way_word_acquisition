dd67f02d2598eeee2018bb25cdad30f8f5fddb1e
Dan Oneata <dan.oneata@gmail.com>
2023-05-17 12:05:42 +0300

    More visualizations

--- scripts/show_mining_performance_flickr.py
+++ scripts/show_mining_performance_flickr.py
@@ -8,6 +8,11 @@ import pandas as pd
 import seaborn as sns
 import streamlit as st
 
+from collections import Counter
+from itertools import groupby
+from functools import partial
+from toolz import assoc
+
 from predict import FlickrEnData, FlickrYoData
 from utils import load
 
@@ -30,8 +35,27 @@ datasets = {
     "yoruba": FlickrYoData(),
 }
 
+LANGUAGES = sorted(datasets.keys())
+
 
 def load_data() -> List[Datum]:
+    def parse_mining_file(lang, line):
+        line = line.strip()
+        parts = line.split(" ")
+        if len(parts) == 1:
+            filename = parts[0]
+            start = None
+            end = None
+        if len(parts) == 3:
+            filename, start, end = parts
+            start = float(start) / 16_000 * 100
+            end = float(end) / 16_000 * 100
+        if lang == "yoruba":
+            fst, *rest = filename.split("_")
+            assert fst == "S001"
+            filename = "_".join(rest)
+        return filename, start, end
+
     def load1(concept, lang):
         folder = datasets["english"].base_metadata_dir
         path = (
@@ -41,7 +65,7 @@ def load_data() -> List[Datum]:
             / "audio_pair_lists"
             / (concept + ".txt")
         )
-        return load(path, lambda line: line.strip())
+        return load(path, partial(parse_mining_file, lang))
 
     def has_concept(lang, concept, filename):
         # if lang == "yoruba" and concept == "bike":
@@ -64,10 +88,12 @@ def load_data() -> List[Datum]:
             "filename": filename,
             "language": lang,
             "caption": datasets[lang].captions[filename],
+            "time-start": start,
+            "time-end": end,
         }
         for concept in concepts
         for lang in languages
-        for rank, filename in enumerate(load1(concept, lang), start=1)
+        for rank, (filename, start, end) in enumerate(load1(concept, lang), start=1)
     ]
 
 
@@ -98,6 +124,11 @@ def main():
     dfg = df.groupby(["concept", "language"])
     df["precision"] = 100 * dfg["has-concept"].cumsum() / df["rank"]
 
+    df_tail = df[df["language"] == "english"].groupby(["concept"]).tail(1)
+    df_tail
+
+    df["language"] = df["language"].replace("yoruba", "yorùbá")
+
     fig = sns.relplot(
         data=df, x="rank", y="precision", hue="concept", col="language", kind="line"
     )
@@ -105,8 +136,105 @@ def main():
     # sns.move_legend(axs[0], "upper left", bbox_to_anchor=(1, 1))
     # fig.tight_layout()
     st.pyplot(fig)
+    path = "output/taslp/mining-precision-flickr-audio-en-yo.png"
+    fig.savefig(path, dpi=300)
 
-    show(data, concept, language)
+    # show(data, concept, language)
+
+    def get_iou(segment1, segment2):
+        start1, end1 = segment1["time-start"], segment1["time-end"]
+        start2, end2 = segment2["time-start"], segment2["time-end"]
+        if start1 > end2 or end1 < start2:
+            return 0
+        else:
+            inter = min(end1, end2) - max(start1, start2)
+            union = max(end1, end2) - min(start1, start2)
+            return inter / union
+
+    def has_concept_aligned(datum, τ=0.25):
+        try:
+            alignment = datasets["english"].alignments[datum["filename"]]
+        except KeyError:
+            alignment = []
+        alignment = [a for a in alignment if a["word"] == datum["concept"]]
+        overlaps = [get_iou(a, datum) for a in alignment]
+        if not overlaps:
+            return False
+        else:
+            return max(overlaps, default=0) >= τ
+
+    data_en = [
+        assoc(datum, "has-concept-aligned", has_concept_aligned(datum))
+        for datum in data
+        if datum["language"] == "english"
+    ]
+    df = pd.DataFrame(data_en)
+    dfg = df.groupby(["concept"])
+    df["precision"] = 100 * dfg["has-concept-aligned"].cumsum() / df["rank"]
+
+    df_tail = df.groupby(["concept"]).tail(1)
+    df_tail
+
+    fig, axs = plt.subplots(ncols=1, squeeze=False)
+    sns.lineplot(data=df, x="rank", y="precision", hue="concept", ax=axs[0, 0])
+    sns.move_legend(axs[0, 0], "upper left", bbox_to_anchor=(1, 1))
+    fig.tight_layout()
+    st.pyplot(fig)
+
+    def get_all_words(datum):
+        try:
+            caption = datasets[datum["language"]].captions[datum["filename"]]
+            return caption.lower().split()
+        except KeyError:
+            return []
+
+    def get_predicted_word(datum):
+        try:
+            alignment = datasets["english"].alignments[datum["filename"]]
+        except KeyError:
+            return None
+        try:
+            return max(alignment, key=lambda a: get_iou(a, datum))["word"]
+        except:
+            pdb.set_trace()
+
+    confusions = [
+        assoc(datum, "word", predicted_word)
+        for datum in data
+        if datum["language"] == "english"
+        and (predicted_word := get_predicted_word(datum))
+    ]
+    for concept, group in groupby(confusions, lambda x: x["concept"]):
+        group = list(group)
+        counter = Counter([x["word"] for x in group])
+        counter_str = [f"{word} {count}" for word, count in counter.most_common(5)]
+        counter_str = ", ".join(counter_str)
+        st.write(concept)
+        st.markdown(counter_str)
+        st.markdown("---")
+
+    st.markdown("### Co-occurences against any word in the caption")
+    confusions = [
+        assoc(datum, "word", word) for datum in data for word in get_all_words(datum)
+    ]
+    for language in LANGUAGES:
+        confusions_lang = [
+            datum for datum in confusions if datum["language"] == language
+        ]
+        st.write(language)
+        for concept, group in groupby(confusions_lang, lambda x: x["concept"]):
+            group = list(group)
+            counter = Counter([x["word"] for x in group])
+            if language == "yoruba":
+                concept_str = "{} ({})".format(
+                    datasets[language].concept_to_yoruba[concept],
+                    concept,
+                )
+            else:
+                concept_str = concept
+            st.write(concept_str)
+            st.write(counter.most_common(20))
+            st.markdown("---")
 
 
 if __name__ == "__main__":
